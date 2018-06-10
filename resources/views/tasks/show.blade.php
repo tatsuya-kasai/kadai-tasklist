0532493d7710d2448f27c3fac622268ece512418
EC2 Default User <ec2-user@ip-172-31-7-4.ec2.internal>
2018-06-10 08:59:18 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
 
-    <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+    <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>{{ $task->content }}</p>
+    <p>text: {{ $task->status }}</p>
+    <p>タスク: {{ $task->content }}</p>
     
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
